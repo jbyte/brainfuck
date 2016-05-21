285099d985aca62afbe59e082b957d2f3b8584c5
Jaka Vute <jv5542@student.uni-lj.si>
2016-05-21 23:54:02 +0200

    Implemented the functionality of the --memory flag

--- bfuck.c
+++ bfuck.c
@@ -8,7 +8,8 @@
 void print_help();
 
 int main(int argc, const char *argv[]){
-    FILE *fp;
+    FILE *fp = NULL;
+    short *mem = (short*)malloc(DEF_MEMORY*sizeof(short));
 
     if (argc==1) {
         print_help();
@@ -21,8 +22,16 @@ int main(int argc, const char *argv[]){
             print_help();
             return 0;
         }else if (strcmp(argv[i],"--memory")==0) {
-            printf("feature not implemented yet\n");
-            return 1;
+            /*printf("feature not implemented yet\n");*/
+            char *tmp = (char*)malloc(strlen(argv[i+1])*sizeof(char));
+            i++;
+            strcpy(tmp,argv[i]);
+            int val = atoi(tmp);
+            if(val){
+                mem = (short*)malloc(val*sizeof(short));
+            }else {
+                printf("The number of bytes you have specified is not valid.\n");
+            }
         }else if (strcmp(argv[i],"--version")==0) {
             printf("bfuck %s\n",VERSION);
         }else if (argc-1>0 && (fp=fopen(argv[i],"r"))) {
@@ -41,7 +50,14 @@ int main(int argc, const char *argv[]){
             return 1;
         }
     }
+    /*printf("%p\n",fp);*/
 
+    if (fp==NULL) {
+        printf("You have not specified a file.\n");
+        return 1;
+    }
+
+    free(mem);
     return 0;
 }
 
