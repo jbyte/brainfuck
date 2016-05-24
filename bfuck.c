a515b754f9a0d22d6fdfbb7c92bd6958c98eabe8
Jaka Vute <jv5542@student.uni-lj.si>
2016-05-24 12:35:58 +0200

    Implemented the logic for brainfuck loops - does not work

--- bfuck.c
+++ bfuck.c
@@ -5,11 +5,34 @@
 #define DEF_MEMORY 1024
 #define VERSION "0.1.alfa"
 
+typedef struct _stack {
+    int elem;
+    struct _stack *next;
+} stack;
+
+void stack_init(stack *st);
+void stack_destroy(stack *st);
+int stack_is_empty(stack *st);
+void stack_push(stack *st, int elem);
+void stack_pop(stack *st);
+
 void print_help();
+char *load_prog(FILE *f);
+int find_match(char *prog, int num);
+
+void execute(char *prog, int memlen, short *mem);
+void exec_move_right(int *mp, int memlen);
+void exec_move_left(int *mp, int memlen);
+void exec_add(int mp, short *mem);
+void exec_sub(int mp, short *mem);
+void exec_write(int mp, short *mem);
+void exec_read(int mp, short *mem);
+int exec_loop(int pp, char *prog, int *mp, int memlen, short *mem);
 
 int main(int argc, const char *argv[]){
     FILE *fp = NULL;
-    short *mem = (short*)malloc(DEF_MEMORY*sizeof(short));
+    short *mem = (short*)calloc(DEF_MEMORY,sizeof(short));
+    int val = 0;
 
     if (argc==1) {
         print_help();
@@ -26,23 +49,24 @@ int main(int argc, const char *argv[]){
             char *tmp = (char*)malloc(strlen(argv[i+1])*sizeof(char));
             i++;
             strcpy(tmp,argv[i]);
-            int val = atoi(tmp);
+            val = atoi(tmp);
             if(val){
-                mem = (short*)malloc(val*sizeof(short));
+                mem = (short*)calloc(val,sizeof(short));
             }else {
                 printf("The number of bytes you have specified is not valid.\n");
             }
         }else if (strcmp(argv[i],"--version")==0) {
             printf("bfuck %s\n",VERSION);
-        }else if (argc-1>0 && (fp=fopen(argv[i],"r"))) {
-            char c;
-            while ((c=fgetc(fp))!=EOF) {
-                printf("%c",c);
-            }
-            fclose(fp);
+        }else if (argc-1>0 && (fp=fopen(argv[i],"rb"))) {
+            /*printf("%s\n");*/
+            /*char c;*/
+            /*while ((c=fgetc(fp))!=EOF) {*/
+                /*printf("%c",c);*/
+            /*}*/
+            /*fclose(fp);*/
         }else {
             if(argv[i][0]=='-'){
-                printf("Unknown command: %s\n",argv[i]);
+                printf("Unknown flag: %s\n",argv[i]);
             }else{
                 printf("Unknown file: %s\n",argv[i]);
             }
@@ -55,10 +79,156 @@ int main(int argc, const char *argv[]){
     if (fp==NULL) {
         printf("You have not specified a file.\n");
         return 1;
+    }else{
+        val = (val>=64) ? val : DEF_MEMORY;
+        char *prog = load_prog(fp);
+        fclose(fp);
+        execute(prog,val,mem);
     }
 
     free(mem);
     return 0;
+}
+
+void execute(char *prog, int memlen, short *mem){
+    int pp = 0;
+    int mp = 0;
+    printf("%s\n",prog);
+    while(pp<strlen(prog)){
+        switch(prog[pp]){
+            case '>':
+                exec_move_right(&mp,memlen);
+                pp++;
+                break;
+            case '<':
+                exec_move_left(&mp,memlen);
+                pp++;
+                break;
+            case '+':
+                exec_add(mp,mem);
+                pp++;
+                break;
+            case '-':
+                exec_sub(mp,mem);
+                pp++;
+                break;
+            case '.':
+                exec_write(mp,mem);
+                /*printf("Writing shit...\n");*/
+                pp++;
+                break;
+            case ',':
+                exec_read(mp,mem);
+                pp++;
+                break;
+            case '[':
+                pp = exec_loop(pp,prog,&mp,memlen,mem);
+                break;
+            case ']':
+                printf("Unmached \']\'\n");
+                exit(1);
+            default:
+                pp++;
+                continue;
+        }
+    }
+}
+
+void exec_move_right(int *mp, int memlen){
+    if(*mp<memlen) (*mp)++;
+    else (*mp) = 0;
+}
+void exec_move_left(int *mp, int memlen){
+    if(*mp>0) (*mp)--;
+    else (*mp) = memlen -1;
+}
+void exec_add(int mp, short *mem){
+    mem[mp]++;
+}
+void exec_sub(int mp, short *mem){
+    mem[mp]--;
+}
+void exec_write(int mp, short *mem){
+    putchar(mem[mp]);
+    printf("Every day im printing...\n");
+}
+void exec_read(int mp, short *mem){
+    mem[mp] = getchar();
+}
+int exec_loop(int pp, char *prog, int *mp, int memlen, short *mem){
+    /*printf("This is only a test...\n");*/
+    int tmp = pp;
+    int match = find_match(prog,pp);
+    if(match==-1){
+        printf("Unmached \'[\'\n");
+        exit(1);
+    }
+    if(mem[*mp]==0) return match+1;
+    else{
+        while(pp<=match){
+            switch(prog[pp]){
+                case '>':
+                    exec_move_right(mp,memlen);
+                    pp++;
+                    break;
+                case '<':
+                    exec_move_left(mp,memlen);
+                    pp++;
+                    break;
+                case '+':
+                    exec_add(*mp,mem);
+                    pp++;
+                    break;
+                case '-':
+                    exec_sub(*mp,mem);
+                    pp++;
+                    break;
+                case '.':
+                    exec_write(*mp,mem);
+                    pp++;
+                    break;
+                case ',':
+                    exec_read(*mp,mem);
+                    pp++;
+                    break;
+                case '[':
+                    pp = exec_loop(pp,prog,mp,memlen,mem);
+                    break;
+                case ']':
+                    if(mem[*mp]!=0) pp = tmp;
+                    else pp++;
+                    break;
+                default:
+                    pp++;
+                    continue;
+            }
+        }
+        return pp;
+    }
+}
+
+int find_match(char *prog, int num){
+    int count = 1;
+    int i = num + 1;
+    for(; i<strlen(prog); i++){
+        if(prog[i]=='[') count++;
+        if(prog[i]==']') count--;
+        if(count==0) return i;
+    }
+    return -1;
+}
+
+char *load_prog(FILE *f){
+    fseek(f,0,SEEK_END);
+    long fsize = ftell(f);
+    fseek(f,0,SEEK_SET);
+
+    char *str = (char*)malloc((fsize+1)*sizeof(char));
+    fread(str,fsize,1,f);
+
+    str[fsize] = 0;
+
+    return str;
 }
 
 void print_help(){
@@ -67,5 +237,29 @@ void print_help(){
     printf("Options:\n");
     printf("\t\t%*s%*s\n",-25,"--help",-125,"displays the help");
     printf("\t\t%*s%*s\n",-25,"--version",-125,"displays the version");
-    printf("\t\t%*s%*s\n",-25,"--memory n",-125,"sets the data memory to n Bytes (default 1024)");
+    printf("\t\t%*s%*s\n",-25,"--memory n",-125,"sets the data memory to n Bytes (default 1024, minimum 64)");
+}
+
+void stack_init(stack *st){
+    st = (stack*)malloc(sizeof(stack));
+}
+void stack_destroy(stack *st){
+    free(st);
+}
+int stack_is_empty(stack *st){
+    return st->next==NULL ? 1 : 0;
+}
+void stack_push(stack *st, int elem){
+    stack *tmp = (stack*)malloc(sizeof(stack));
+    tmp->elem = elem;
+    tmp->next = st->next;
+    st->next = tmp;
+}
+void stack_pop(stack *st){
+    if(stack_is_empty(st)){
+        printf("You're popping an empty stack!\n");
+        exit(1);
+    }
+    stack *tmp = st->next->next;
+    st->next = tmp;
 }
