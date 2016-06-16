9538b532fa5f072de632da4208f83f0b64b17969
Jaka Vute <jv5542@student.uni-lj.si>
2016-06-16 16:19:53 +0200

    Done some code and TODO comments.

--- bfuck.c
+++ bfuck.c
@@ -1,37 +1,59 @@
+/**************************** includes **************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 /*#include <windows.h>*/
 /*#include <unistd.h>*/
 
-#define DEF_MEMORY 1024
+/**************************** const definitions *****************************/
+#define DEF_MEMORY 1024     //default program data memory
 #define VERSION "1.0"
 #define TRUE 1
 #define FALSE 0
 
-void print_help();
-void write_mem_to_file(short *mem, int len, FILE *file);
-void write_inst_to_file(char c, FILE *file);
-char *load_prog(FILE *f);
-int find_match(char *prog, int num);
+/*************************** type and struct definitions *******************/
+typedef struct _stack{      // a linked-list implementation of a stack of ints
+    int elem;
+    struct _stack *next;
+}stack;
 
-void execute(char *prog, int memlen, short *mem, int debug, FILE *debug_fp);
-void exec_move_right(int *mp, int memlen);
-void exec_move_left(int *mp, int memlen);
-void exec_add(int mp, short *mem);
-void exec_sub(int mp, short *mem);
-void exec_write(int mp, short *mem);
-void exec_read(int mp, short *mem);
+/*************************** function prototypes ***************************/
+void print_help();                                              // interpreter help
+// debug help functions
+void write_mem_to_file(short *mem, int len, FILE *file);        // if flag --debug, writes memory to file
+void write_inst_to_file(char c, FILE *file);                    // if flag --debug, writes current instructio to file
+
+// helper functions for the execution
+char *load_prog(FILE *f);                   // loading the contents of a file to a string
+int find_match(char *prog, int num);        // find matching square brackets (brainfuck loop)
+
+// stack functions
+*stack stack_init();                        // returns a new stack
+void stack_push(*stack stck, int elem);     // push an element on the stack
+int stack_pop(*stack stck);                 // pop an element from the stack
+
+// brainfuck program execution functions
+void execute(char *prog, int memlen, short *mem, int debug, FILE *debug_fp);    // the execution loop
+void exec_move_right(int *mp, int memlen);                                      // exec '>'
+void exec_move_left(int *mp, int memlen);                                       // exec '<'
+void exec_add(int mp, short *mem);                                              // exec '+'
+void exec_sub(int mp, short *mem);                                              // exec '-'
+void exec_write(int mp, short *mem);                                            // exec '.'
+void exec_read(int mp, short *mem);                                             // exec ','
+// TODO: replace recursive loop implementation with a non-stack-overflowing one
 int exec_loop(int pp, char *prog, int *mp, int memlen, short *mem, int debug, FILE *debug_fp);
 
+/***************************** the main program *****************************/
 int main(int argc, const char *argv[]){
     FILE *fp = NULL;
     FILE *debug_fp = NULL;
+    // TODO: probably unsigned short?
     short *mem = (short*)calloc(DEF_MEMORY,sizeof(short));
     int val = 0;
     int debug = FALSE;
 
     if (argc==1) {
+        // if called without arguments print help info and exit
         print_help();
         return 1;
     }
@@ -39,19 +61,24 @@ int main(int argc, const char *argv[]){
     int i;
     for (i = 1; i < argc; i++) {
         if (strcmp(argv[i],"--help")==0) {
+            // handle --help flag
             print_help();
             return 0;
-        }else if (strcmp(argv[i],"--memory")==0) {
+        }else if (strcmp(argv[i],"--memory")==0) { // TODO: handle argv[i+1]==NULL case
+            // handle --memory flag
             char *tmp = (char*)malloc(strlen(argv[i+1])*sizeof(char));
             i++;
             strcpy(tmp,argv[i]);
+            // create new memory pointer of size val or print error and exit
             val = atoi(tmp);
             if(val){
                 mem = (short*)calloc(val,sizeof(short));
             }else {
                 printf("The number of bytes you have specified is not valid.\n");
+                return 1;
             }
-        }else if(strcmp(argv[i],"--debug")==0){
+        }else if(strcmp(argv[i],"--debug")==0){ //TODO: handle the 'no specified file' error
+            // handle --debug flag
             debug = TRUE;
             i++;
             debug_fp = fopen(argv[i],"w");
@@ -59,6 +86,7 @@ int main(int argc, const char *argv[]){
             printf("bfuck %s\n",VERSION);
         }else if(argc-1>0 && (fp=fopen(argv[i],"rb"))) {
         }else{
+            // handle unknown flags and file names
             if(argv[i][0]=='-'){
                 printf("Unknown flag: %s\n",argv[i]);
             }else{
@@ -73,6 +101,7 @@ int main(int argc, const char *argv[]){
         printf("You have not specified a file.\n");
         return 1;
     }else{
+        // TODO: wat? Move memory allocation here
         val = (val>=64) ? val : DEF_MEMORY;
 
         char *prog = load_prog(fp);
@@ -90,15 +119,16 @@ int main(int argc, const char *argv[]){
 }
 
 void execute(char *prog, int memlen, short *mem, int debug, FILE *debug_fp){
-    int pp = 0;
-    int mp = 0;
+    int pp = 0;             // program pointer - index of the current instruction character in program string
+    int mp = 0;             // memory pointer - index of the currently accesible memory field
     printf("%s\n",prog);
 
-    const char instrs[] = "<>+-[],.";
+    const char instrs[] = "<>+-[],."    // TODO: move to global scope
 
     while(pp<strlen(prog)){
-        char tmp = prog[pp];
+        char tmp = prog[pp];            // temporary variable for storing the program pointer for --debug flag
         switch(prog[pp]){
+            // handling of the various brainfuck instructions
             case '>':
                 exec_move_right(&mp,memlen);
                 pp++;
@@ -124,9 +154,11 @@ void execute(char *prog, int memlen, short *mem, int debug, FILE *debug_fp){
                 pp++;
                 break;
             case '[':
+                // TODO: handle looping w/o recursion
                 pp = exec_loop(pp,prog,&mp,memlen,mem,debug,debug_fp);
                 break;
             case ']':
+                // TODO: handle looping w/o recursion
                 printf("Unmached \']\'\n");
                 exit(1);
             default:
@@ -134,6 +166,7 @@ void execute(char *prog, int memlen, short *mem, int debug, FILE *debug_fp){
         }
         if(debug && strchr(instrs,tmp)!=NULL){
             write_inst_to_file(tmp,debug_fp);
+            // TODO: add memory print after reading from stdin (or file)
             if(tmp=='+' || tmp=='-'){
                 write_mem_to_file(mem,memlen,debug_fp);
             }
@@ -157,12 +190,11 @@ void exec_sub(int mp, short *mem){
 }
 void exec_write(int mp, short *mem){
     putchar(mem[mp]);
-    /*sleep(1);*/
-    /*printf("Every day im printing...\n");*/
 }
 void exec_read(int mp, short *mem){
     mem[mp] = getchar();
 }
+// TODO: remove after better loop implementation
 int exec_loop(int pp, char *prog, int *mp, int memlen, short *mem, int debug, FILE *debug_fp){
     int tmp = pp;
     int match = find_match(prog,pp);
@@ -237,6 +269,7 @@ int find_match(char *prog, int num){
     return -1;
 }
 
+// TODO: maybe move elsewhere in file?
 char *load_prog(FILE *f){
     fseek(f,0,SEEK_END);
     long fsize = ftell(f);
@@ -247,16 +280,6 @@ char *load_prog(FILE *f){
 
     str[fsize] = 0;
     return str;
-}
-
-void print_help(){
-    printf("Usage:\n");
-    printf("\t\tbfuck [options] <file>\n");
-    printf("Options:\n");
-    printf("\t\t%*s%*s\n",-25,"--help",-125,"displays the help");
-    printf("\t\t%*s%*s\n",-25,"--version",-125,"displays the version");
-    printf("\t\t%*s%*s\n",-25,"--debug",-125,"writes the debug info into a file");
-    printf("\t\t%*s%*s\n",-25,"--memory n",-125,"sets the data memory to n Bytes (default 1024, minimum 64)");
 }
 
 void write_mem_to_file(short *mem, int len, FILE *file){
@@ -272,4 +295,15 @@ void write_mem_to_file(short *mem, int len, FILE *file){
 
 void write_inst_to_file(char c, FILE *file){
     fprintf(file, "Instruction : %c\n", c);
+}
+
+void print_help(){
+    printf("Usage:\n");
+    printf("\t\tbfuck [options] <file>\n");
+    printf("Options:\n");
+    printf("\t\t%*s%*s\n",-25,"--help",-125,"displays the help");
+    printf("\t\t%*s%*s\n",-25,"--version",-125,"displays the version");
+    printf("\t\t%*s%*s\n",-25,"--debug",-125,"writes the debug info into a file");
+    printf("\t\t%*s%*s\n",-25,"--memory n",-125,"sets the data memory to n Bytes (default 1024, minimum 64)");
+    // TODO: maybe add examples?
 }
